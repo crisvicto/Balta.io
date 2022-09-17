90f1d8091b8cdfbfab8d0d2d81ff3e5761e62fcd
Cristiane Victorino Ferreira <cristiane_victorino@hotmail.com>
2022-09-17 17:13:49 -0300

    Soma
    Aprendendo a soma de dois valores

---
+++ Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine()); //Parse para conversão de valor em string  
+
+            Console.WriteLine(""); 
+
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado da soma é: " + resultado);
+
+            /*
+            Interpolação de Strings
+            Console.WriteLine($"O resultado da soma é: {resultado}");  
+            Console.WriteLine($"O resultado da soma é: {v1 + v2}");
+            Utilizando parenteses
+            Console.WriteLine("O resultado da soma é: " + (v1 + v2));
+            */
+
+            
+
+        }
+    }
+}
